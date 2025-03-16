53a69d47d17fb223f3ede5ffc2b58df28cfd8eba
zhangxinxin <1285367184@qq.com>
2025-03-16 19:15:46 +0800

    20.Express如何使用multer实现文件上传

---
+++ 20.express如何使用multer实现文件上传/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const multer = require('multer')
+const cors = require('cors');
+
+const app = express()
+app.use(cors());
+
+const upload = multer({ dest: 'uploads/' })
+
+app.post('/aaa', upload.single('aaa'), function (req, res, next) {
+	console.log('req.file', req.file);
+	console.log('req.body', req.body);
+})
+
+app.listen(3333);
